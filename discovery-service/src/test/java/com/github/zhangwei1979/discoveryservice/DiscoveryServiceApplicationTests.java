7a39e53bc012082ea67627849e7e44f260b9de60
Wei Zhang <zhangwei1979@outlook.com>
2022-01-06 16:21:22 +0800

    first commit

---
+++ discovery-service/src/test/java/com/github/zhangwei1979/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zhangwei1979.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/github/zhangwei1979/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zhangwei1979.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
